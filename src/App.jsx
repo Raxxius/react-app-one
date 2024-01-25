d46d07c7f0290c49ff10acec27cc007dcc0d90ce
Raxxius <raxxman@gmail.com>
2024-01-25 18:34:15 +0000

    reduced to core return

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,34 @@ import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  // State function
-  const [colour, setColour] = useState("green")
+  // // State function
+  // const [colour, setColour] = useState("green")
 
-  console.log("App one loading")
-  console.log(sessionStorage.currentColour)
-  console.log(colour)
+  // console.log("App one loading")
+  // console.log(sessionStorage.currentColour)
+  // console.log(colour)
 
-  // Setup a listener for Session Storage changes
-  useEffect(() => {
-    setColour(sessionStorage.currentColour)
-    const handleStorageChange = () => {
-        setColour(sessionStorage.currentColour);
-        console.log("storage changed")
-    };
+  // // Setup a listener for Session Storage changes
+  // useEffect(() => {
+  //   setColour(sessionStorage.currentColour)
+  //   const handleStorageChange = () => {
+  //       setColour(sessionStorage.currentColour);
+  //       console.log("storage changed")
+  //   };
 
-    // Event listener
-    window.addEventListener('storage', handleStorageChange);
-    console.log("event listener engaged")
+  //   // Event listener
+  //   window.addEventListener('storage', handleStorageChange);
+  //   console.log("event listener engaged")
 
-    // Cleanup
-    return () => {
-      window.removeEventListener('storage', handleStorageChange);
-    };
-  }, []);
+  //   // Cleanup
+  //   return () => {
+  //     window.removeEventListener('storage', handleStorageChange);
+  //   };
+  // }, []);
 
   // Core return 
   return (
-    <div style={{ backgroundColor: colour, width: '100px', height: '100px' }}></div>
+    <div style={{ backgroundColor: "green", width: '100px', height: '100px' }}></div>
   )
 
 }
